f73946d479939e2c315d7ca33bcd1060d1d551db
Shaurya Garg <sgar5840@uni.sydney.edu.au>
2026-08-21 14:35:06 +1000

    Create helper.py

---
+++ helper.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.constants as con
+from scipy.special import erf, erfi
+from numpy import sqrt, pi, exp
+import scipy
+from scipy.optimize import fsolve
+
+
+def speed(v):
+    return np.linalg.norm(v, axis = 1)
+
+def Nesc_gaussian(sig, v_esc):
+    # Got this from github.com/cajohare/DarkShards/blob/master/code/HaloFuncs.py
+    # sig = 1d dispersion
+    # v_esc = escape velocity
+    return erf(v_esc/(sqrt(2)*sig)) - \
+    sqrt(2.0/pi)*(v_esc/sig)*exp(-v_esc**2.0/(2.0*sig**2.0))
+
+def fv_gaussian(v, sig, v_c, v_esc = 528.0):
+    constants = 2 * pi * sig**2
+    constants = 1 / (np.pow(constants, 3/2))
+    
+    N_esc = Nesc_gaussian(sig, v_esc)
+    
+    f = exp(-(speed(v - v_c)**2) / 2 / (sig**2)) / N_esc
+    
+    return constants * f
+
+def speedDistGaussian(v, sig, v_c, v_esc= 528.0):
+    constants = v / (sqrt(2*pi) * v_c * sig)
+    
+    # got this from ciaran's fine grained dark matter paper, is v_e supposed to be v_c?
+    f1 = exp(- (v**2 + v_c**2 - 2*v*v_c) / (2 * sig**2))
+    f2 = exp(- (v**2 + v_c**2 + 2*v*v_c) / (2 * sig**2))
+    
+    N_esc = Nesc_gaussian(sig, v_esc) # what is N_esc?
+    #print(N_esc)
+    
+    return (f1 - f2) * constants / N_esc
+
+def numStreams(rho):
+    return scipy.stats.skewnorm.pdf(np.log(rho),-2.10,loc=-11.37,scale=7.15)/rho
+
+def get_density(str_num):
+    # returns the optimum stream density to get 
+
+    def fun(rho, str_num):
+        return np.abs(numStreams(rho) - str_num)
+
+    return scipy.optimize.minimize(fun, x0 = 2.04e-3, bounds = [(7.7e-8, np.inf)], args=[str_num]).x[0]
